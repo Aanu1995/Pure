75a201bb447bb6f885b9f8ab5af83143d0a777ba
Aanu Olakunle <olakunleaanu@gmail.com>
2021-11-26 14:38:14 +0100

    key

--- lib/repositories/algolia_application.dart
+++ lib/repositories/algolia_application.dart
@@ -7,8 +7,8 @@ class AlgoliaApplication {
     switch (F.appFlavor) {
       case Flavor.prod:
         return Algolia.init(
-          applicationId: '',
-          apiKey: '',
+          applicationId: 'R0547NV5EC',
+          apiKey: '536c516e3a6d13732b71017fa635dcfa',
         );
       case Flavor.staging:
         return Algolia.init(
